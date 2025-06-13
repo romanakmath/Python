644a2e6c0e88e7e13f4046cbd7f6195a79f28944
marcor78R <marcorodehueser@gmx.de>
2025-06-13 12:10:28 +0200

    test2

--- database/test_imp_yfinance.py
+++ database/test_imp_yfinance.py
@@ -2,7 +2,7 @@ from database.get_yfinance_data import get_yfinance_data
 import datetime as d
 
 
-symbol = 'Microsft'
-startPoint = '01.01.2025'
+symbol = 'Microsoft'
 period = '1m'
+startPoint = '01.01.2025'
 get_yfinance_data(symbol, period, startPoint)
