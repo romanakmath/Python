614d0cd9459bd46f03189b33d31df05ca6639771
marcor78R <marcorodehueser@gmx.de>
2025-06-13 15:59:01 +0200

    Erstellen Basis Tabelle

--- database/get_yfinance_data.py
+++ database/get_yfinance_data.py
@@ -1,25 +1,33 @@
 import yfinance as yf
+import openpyxl as ox
 import pandas as pd
+import sqlite3 
 
 def  get_yfinance_data(symbol, period, startPoint):
-    dat = yf.Ticker("MSFT")
-    dat.info
-    dat.calendar
-    dat.analyst_price_targets
-    dat.quarterly_income_stmt
-    dat.history(period='1mo')
-    dat.option_chain(dat.options[0]).calls
+    #  dat = yf.Ticker("MSFT")
+    # # dat.info
+    # # dat.calendar
+    # # dat.analyst_price_targets
+    # # dat.quarterly_income_stmt
+    # # dat.history(period='1mo')
+    # # dat.option_chain(dat.options[0]).calls
 
-    pfinancials = dat.financials
+    #  pfinancials = dat.financials
 
-    #print(pfinancials)
+     #print(pfinancials)
 
-    print(dat.history(period=period))
+    # print(dat.history(period=period))
 
-    data = yf.download(list(dat), group_by='column', period=period, interval='1d')
+    data = yf.download(list(symbol), interval='1d')
 
-    print(data)
-    #print(data['Open'])
+    #print(data)
+    #print(type(data))
+    #print(data['Close'])
+
+    #data.to_csv (r'C:\BckUp\Python\finance.csv', index = None, header=True) 
+
+    conn = sqlite3.connect('backtest.db')
+    data.to_sql('finance', conn, if_exists='replace')
 
     
 
