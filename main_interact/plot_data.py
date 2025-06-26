aa930285dffef95b717e913680ed88cf255e0cbf
marcor78R <marcorodehueser@gmx.de>
2025-06-26 11:05:55 +0200

    Plot Kerzen Diagramm für price

--- main_interact/plot_data.py
+++ main_interact/plot_data.py
@@ -1,11 +1,33 @@
 # data_df hat die ganze Info über Open, Close, High, Low, Volume, Tag
 import time
 import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
+import mplfinance as mpf
 
 
 def plot_data(symbol, data_df, **time_points):
     start_point = time_points["start"]
     end_point = time_points["end"]
     
-    data_df.plot(kind="scatter", x="date", y="open")
-    plt.show()
+    # data_df.plot(kind="scatter", x="date", y="open")
+    # plt.show()
+
+    temp =  data_df['open']
+    temp2= list(data_df['open'])
+
+
+    data = {
+     'Open': list(data_df['open']),
+     'High': list(data_df['high']),
+     'Low':  list(data_df['low']),
+     'Close':  list(data_df['close'])
+    }
+
+    time_index = pd.DatetimeIndex(data_df['date'])
+
+    df2 = pd.DataFrame(data, index=time_index)
+
+    mpf.plot(df2, type='candle', style='yahoo',
+         title='Sample Candlestick Chart',
+         ylabel='Price')
